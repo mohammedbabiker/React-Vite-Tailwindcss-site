7ae21fdfa89aa8695803349149ef41b4a6ee93a2
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-08-23 13:02:47 +0300

    sidebar components all good

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
     <>
       <Header />
+      <SideBar />
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ function Header() {
       <div className="w-[90px] h-[90px] bg-black">
         <img src="./logo.png" className="p-7" />
       </div>
-      <div className="flex gap-14">
+      <div className="hidden md:flex gap-14">
         {menu.map((item) => (
           <div className="cursor-pointer font-medium hover:underline">
             <h2>{item.name}</h2>
