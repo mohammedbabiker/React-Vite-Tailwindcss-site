2bddb051ba6109c20dc969bbbb3c8eb484bff277
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-08-23 14:00:34 +0300

    update user photo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import "./App.css";
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
+import ProfilePic from "./components/ProfilePic";
 
 function App() {
   return (
     <>
       <Header />
       <SideBar />
+      <div className="ml-[90px] grid grid-cols-1 md:grid-cols-3">
+        <div className="col-span-2">User Data</div>
+        <div className="hidden md:block">
+          <ProfilePic />
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/ProfilePic.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div className="fixed">
+      <img
+        src="https://images.pexels.com/photos/16129705/pexels-photo-16129705/free-photo-of-man-sitting-at-desk-working-on-computers.jpeg?auto=compress&cs=tinysrgb&w=1600"
+        className="object-cover h-screen"
+      />
+    </div>
+  );
+}
+
+export default Profile;
