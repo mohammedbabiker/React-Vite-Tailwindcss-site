80a33c67abdc46472782969f7f31e904fafa5337
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-08-23 12:24:30 +0300

    header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <div className="card">
-        <h1>man</h1>
-      </div>
+      <Header />
     </>
   );
 }
